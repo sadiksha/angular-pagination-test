b25466f673fdf20feef8593dfd30c4422cd8787d
Sadiksha Gautam <sadiksha.gautam@ericsson.com>
2017-03-08 14:22:37 +0100

    Refactors the controller code in app.js. Separates definition and usage.

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+(function(){
+
+  angular.module('paginationExample',['ui.bootstrap'])
+  .controller('ProfessionalController', ProfessionalController);
+
+  ProfessionalController.$inject = ['$scope', '$rootScope'];
+
+  function ProfessionalController($scope, $rootScope){
+
+    var professionalDetails=[
+      {"name":"Test 1", "position":"Tester",                "salary":"2000"},
+      {"name":"Test 2", "position":"Analyzer",              "salary":"1000"},
+      {"name":"Test 3", "position":"Junior Testter",        "salary":"2000"},
+      {"name":"Test 3", "position":"Junior Analyzer",       "salary":"1000"},
+      {"name":"Test 3", "position":"Tester",                "salary":"2000"},
+      {"name":"Test 3", "position":"Analyser",              "salary":"1000"},
+      {"name":"Test 3", "position":"Integration Engineer",  "salary":"2000"},
+      {"name":"Test 3", "position":"Solution Architect",    "salary":"1000"},
+      {"name":"Test 3", "position":"Software Engineer",     "salary":"2000"},
+      {"name":"Test 3", "position":"Designer",              "salary":"2000"},
+      {"name":"Test 3", "position":"Tester",                "salary":"3000"},
+      {"name":"Test 3", "position":"Analyzer",              "salary":"1000"},
+      {"name":"Test 3", "position":"Architect",             "salary":"5000"},
+      {"name":"Test 3", "position":"Tester",                "salary":"1000"},
+      {"name":"Test 3", "position":"Tester",                "salary":"2000"},
+    ];
+    $scope.filteredRows = [];
+    $scope.currentPage = 1;
+    $scope.numPerPage = 10;
+    $scope.maxSize = 5;
+
+    $scope.makeRows = function() {
+      $scope.rows = [];
+      for (i=0;i<=professionalDetails.length-1;i++) {
+        $scope.rows.push(professionalDetails[i]);
+      }
+    }
+
+    $scope.makeRows();
+    $rootScope.$watch('currentPage + numPerPage', function() {
+      var begin = (($scope.currentPage - 1) * $scope.numPerPage)
+      , end = begin + $scope.numPerPage;
+
+      $scope.filteredRows = $scope.rows.slice(begin, end);
+    });
+  }
+
+
+})();
